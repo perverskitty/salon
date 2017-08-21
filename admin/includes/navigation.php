58ad1494436950d5e5a2fa9ad9700e652aa183d0
Peter Cheung <perverskitty@gmail.com>
2017-08-21 19:35:29 +0100

    Cutting header, side navigation, and footer html code from admin index.php file and pasting into header, navigation and footer php files in admin includes folder.

---
+++ admin/includes/navigation.php
@@ -0,0 +1,58 @@
+    <div class="col-md-3 sidebar">
+      <nav class="sidebar-nav">
+       
+       
+        <!-- sidebar header and logo non-collapsable -->
+        <div class="sidebar-header">
+          <button class="nav-toggler nav-toggler-md sidebar-toggler" type="button" data-toggle="collapse" data-target="#nav-toggleable-md">
+            <span class="sr-only">Toggle nav</span>
+          </button>
+          <a class="sidebar-brand img-responsive" href="../index.html">
+            <span class="icon icon-scissors sidebar-brand-icon"></span>
+          </a>
+        </div>
+       
+         
+        <!-- sidebar search and list of links collapsable -->
+        <div class="collapse nav-toggleable-md" id="nav-toggleable-md">
+          <form class="sidebar-form">
+            <input class="form-control" type="text" placeholder="Search...">
+            <button type="submit" class="btn-link">
+              <span class="icon icon-magnifying-glass"></span>
+            </button>
+          </form>
+            
+          <ul class="nav nav-pills nav-stacked flex-column">
+            <li class="nav-header">Dashboards</li>
+            <li class="nav-item">
+              <a class="nav-link " href="../index.html">Overview</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="../order-history/index.html">Order history</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link "href="../fluid/index.html">Fluid layout</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="../icon-nav/index.html">Icon nav</a>
+            </li>
+            <li class="nav-header">More</li>
+              <li class="nav-item">
+                <a class="nav-link "href="../docs/index.html">Toolkit docs</a>
+              </li>
+            <li class="nav-item">
+              <a class="nav-link" href="http://getbootstrap.com" target="blank">Bootstrap docs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="../index-light/index.html">Light UI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#docsModal" data-toggle="modal">Example modal</a>
+            </li>
+          </ul>
+          <hr class="visible-xs mt-3">
+        </div> <!-- end of sidebar search and list of links collapsable -->
+        
+        
+      </nav>
+    </div> <!-- end of sidebar navigation --> 
