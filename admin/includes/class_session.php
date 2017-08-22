c5887a85cae031572a0ac76e53c8b7a9b9a9fbe7
Peter Cheung <perverskitty@gmail.com>
2017-08-22 15:27:32 +0100

    Adding class properties and a construct method for admin/includes/class_session.php.

--- admin/includes/class_session.php
+++ admin/includes/class_session.php
@@ -4,11 +4,23 @@
 class Session {
   
   
+  private $signed_in;
+  public $user_id;
+  
+  
+  // constructor starts a session
+  function __construct() {
+    session_start();
+  }
   
   
   
   
 } // end of class
+
+
+// instantiate
+$session = new Session();
 
 
 
