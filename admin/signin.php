1c0886ac02f7c8ab340b4d527977d4abfc9e5e20
Peter Cheung <perverskitty@gmail.com>
2017-08-22 16:14:30 +0100

    Adding initial PHP code for admin/signin.php that checks for  submit and then email, password, for checking user credentials against records held in database.

--- admin/signin.php
+++ admin/signin.php
@@ -1,17 +1,27 @@
+<?php require_once("includes/init.php"); ?>
+
+<?php if ($session->is_signed_in()) { redirect("index.php"); } ?>
+
 <?php
 
-
-
-echo "Hello";
-
-
-
-
-
-
-
-
-
-
+if (isset($_POST['submit'])) {
+  
+  $email = trim($_POST['email']);
+  $password = trim($_POST['password']);
+  
+  // method to check the database for user
+  
+  if ($user_found) {
+    $session->login($user_found);
+    redirect("index.php");
+  } else {
+    $the_message = "The email/password combination was not found";
+  }
+  
+} else {
+  
+  $email = "";
+  $password = "";
+}
 
 ?>
