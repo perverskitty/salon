d8799b7e622e8ebf3a911c25cdbd4abad25a7e8a
Peter Cheung <perverskitty@gmail.com>
2017-08-22 12:19:00 +0100

    Creating admin/includes/class_database.php for opening a database connection, passing through queries, escaping strings, and returning insert ids.

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,18 +6,5 @@ define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'salon_db');
 
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// test connection works
-if($connection) {
-  echo 'true';
-}
-
-
-
-
-
-
-
 
 ?>
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,9 +1,10 @@
 <?php
 
+// database connection constants
 include("config.php");
 
-
-
+// classes
+include("class_database.php");
 
 
 
