6dbecc9289a8130af4f1045657bf4766348975de
Peter Cheung <perverskitty@gmail.com>
2017-08-22 15:44:25 +0100

    Creating methods for admin/includes/class_session.php for checking a session, getter for signed_in private property, and for signing in and out a user.

--- admin/includes/class_session.php
+++ admin/includes/class_session.php
@@ -3,17 +3,51 @@
 
 class Session {
   
-  
-  private $signed_in;
+  // properties
+  private $signed_in = false;
   public $user_id;
   
   
   // constructor starts a session
   function __construct() {
     session_start();
+    $this->check_the_signin();
   }
   
   
+  // getter method for $signed_in property
+  public function is_signed_in() {
+    return $this->$signed_in;
+  }
+  
+  
+  // sign in a user
+  public function signin($user) {
+    if ($user) {
+      $this->user_id = $_SESSION['user_id'] = $user->id;
+      $this->signed_in = true;
+    }
+  }
+  
+  
+  // sign out a user
+  public function signout($user) {
+    unset($_SESSION['user_id']);
+    unset($this->user_id);
+    $this->signed_in = false;
+  }
+  
+  
+  // check if a session is set 
+  private function check_the_signin() {
+    if (isset($_SESSION['user_id'])) {
+      $this->user_id = $_SESSION['user_id'];
+      $this->signed_in = true;
+    } else {
+      unset($this->user_id);
+      $this->signed_in = false;
+    }
+  }
   
   
 } // end of class
