bb86f8f8fe56a0e9b7b49578c402898bf4b90763
Peter Cheung <perverskitty@gmail.com>
2017-08-22 15:13:34 +0100

    Adding class_autoloader() function to admin/includes/functions.php for autoloading undeclared classes using spl_autoload_register() in PHP.

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,7 +1,17 @@
 <?php
 
 
-
+// autoloads undeclared classes
+function class_autoloader($class) {
+  $class = strtolower($class);
+  $the_path = "includes/class_{$class}.php";
+  
+  // if a regular file and class has not been defined  
+  if (is_file($the_path) && !class_exists("class_{$class}")) {
+    require_once($the_path);
+  } 
+}
+spl_autoload_register('class_autoloader');
 
 
 
--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,13 +1,14 @@
 <?php
 
+// include functions
+require_once("functions.php");
+
 // include database connection constants
 require_once("config.php");
 
 // include classes
 require_once("class_database.php");
 require_once("class_user.php");
-
-
 
 
 ?>
