f744d8bd59791411aacb2a2f81847b26e637ad65
Peter Cheung <perverskitty@gmail.com>
2017-08-23 12:45:54 +0100

    Creating count_all() method for Db_object class (admin/includes/class_db_object.php) for returning count of records for a db table.

--- admin/includes/class_db_object.php
+++ admin/includes/class_db_object.php
@@ -122,6 +122,16 @@ class Db_object {
   }
   
   
+  // returns count of records in a db table
+  public static function count_all() {
+    global $database;
+    $sql = "SELECT COUNT(*) FROM " . static::$db_table;
+    $result_set = $database->query($sql);
+    $row = mysqli_fetch_array($result_set);
+    return array_shift($row);
+  }
+  
+  
 } // end of class
 
 ?>
