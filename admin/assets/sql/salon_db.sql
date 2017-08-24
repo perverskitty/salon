7e7ab78e7512ef3bdbbc3a131adc55385218002b
Peter Cheung <perverskitty@gmail.com>
2017-08-24 18:28:59 +0100

    Saving schema for Services table in admin/assets/sql/salon_db.sql.

--- admin/assets/sql/salon_db.sql
+++ admin/assets/sql/salon_db.sql
@@ -11,22 +11,21 @@ CREATE TABLE `salon_db`.`users` (
   `tel` VARCHAR(30) NOT NULL ,
   `gender` INT(3) NOT NULL ,
   `role_id` INT(3) NOT NULL ,
-  `hairdresser_id` INT(11) NULL DEFAULT NULL ,
+  `hairdresser_id` INT(11) NULL ,
   `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   `changed_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
 
--- example insert users data
-INSERT INTO `users` 
-  (`id`, `first_name`, `last_name`, `email`, `password`, `tel`, `gender`, `role_id`, `hairdresser_id`, `created_at`, `changed_at`) 
-  VALUES 
-  (NULL, 'Peter', 'Cheung', 'peter@test.com', 'peter', '07540 346 882', '1', '1', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-  
-INSERT INTO `users` 
-  (`id`, `first_name`, `last_name`, `email`, `password`, `tel`, `gender`, `role_id`, `hairdresser_id`, `created_at`, `changed_at`)
-  VALUES 
-  (NULL, 'David', 'Cheung', 'david@test.com', 'david', '07540 333 444', '1', '2', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-
+-- Services table
+CREATE TABLE `salon_db`.`services` ( 
+  `id` INT(11) NOT NULL AUTO_INCREMENT , 
+  `name` VARCHAR(255) NOT NULL , 
+  `duration` TIME NOT NULL , 
+  `category_id` INT(3) NOT NULL , 
+  `cost` DECIMAL(8,2) NOT NULL , 
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+  `changed_at` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
