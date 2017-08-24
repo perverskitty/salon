f217d4e7cf1531817d5529ace76c927d25286004
Peter Cheung <perverskitty@gmail.com>
2017-08-24 10:59:44 +0100

    Creating admin/client_edit.php page for updating a client's record, and admin/client_add.php for adding a new client to the db.

---
+++ admin/client_edit.php
@@ -0,0 +1,46 @@
+<?php include("includes/header.php"); ?>
+     
+<?php if(!$session->is_signed_in()) { redirect("signin.php"); } ?>
+     
+<?php
+
+
+?>
+      
+    <!-- Main content --> 
+    <div class="col-md-9 content">
+      
+      
+      <!-- Dash title -->  
+      <div class="dashhead">  
+        <div class="dashhead-titles">
+          <h6 class="dashhead-subtitle">Admin</h6>
+          <h2 class="dashhead-title">Edit client</h2>
+        </div>
+      </div> <!-- end of dash title -->  
+      
+      
+      <!-- Dash table search and action buttons -->
+      <div class="flextable table-actions">
+        <!-- Search orders input -->
+        <div class="flextable-item flextable-primary">
+          <div class="btn-toolbar-item input-with-icon">
+            <input type="text" class="form-control input-block" placeholder="Search clients">
+            <span class="icon icon-magnifying-glass"></span>
+          </div>
+        </div> <!-- end of search orders input -->
+        <div class="flextable-item">
+          <div class="btn-group">
+            <button type="button" class="btn btn-outline-primary" onclick="window.location='add_client.php'">
+              <span class="icon icon-add-user"></span> Add client
+            </button>
+          </div>
+        </div>
+      </div> <!-- end of dash table search and action buttons -->
+
+     
+
+
+      </div> <!-- end of main content -->
+      
+<?php include("includes/footer.php"); ?>
