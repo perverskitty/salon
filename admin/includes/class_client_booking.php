fc1277b5d502ce62af5ed5a7db897650759f31ac
Peter Cheung <perverskitty@gmail.com>
2017-08-28 10:52:13 +0100

    Updating class properties for Client_booking class (admin/includes/class_client_booking.php).'

--- admin/includes/class_client_booking.php
+++ admin/includes/class_client_booking.php
@@ -3,12 +3,10 @@
 class Client_booking extends Booking {
   
   // properties
-  protected static $db_table = "client_bookings";
-  protected static $db_table_fields = array('id', 'client_id', 'service_id');
+  protected static $db_table = "bookings";
+  protected static $db_table_fields = array('booking_date', 'start_time', 'end_time', 'hairdresser_id', 'activity_id', 'booking_text', 'client_id', 'service_id');
   public $client_id;
   public $service_id;
-  public $created_at;
-  public $changed_at;
   
   
   
