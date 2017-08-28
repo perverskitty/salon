67a5ce3bc92cd68aece749f36939422c9100b524
Peter Cheung <perverskitty@gmail.com>
2017-08-28 11:39:57 +0100

    Copying methods from Booking class to Client_booking class, and to update later. Added comments to validate() method making it easier to understand.

--- admin/includes/class_booking.php
+++ admin/includes/class_booking.php
@@ -14,57 +14,69 @@ class Booking extends Db_object {
   public $booking_text;
   
   
-  // validate method
+  // validate form inputs and check avalability
   public function validate() {
     date_default_timezone_set("Europe/London");
     $date_today = date("Y-m-d");
     $time_today = date("H:i:s");
     
+    // user did not select a hairdresser
     if ($this->hairdresser_id == 0) {
       Message::setMsg("Please select a hairdresser", "error");
       return false;
     } 
+    // user did not select an activity
     if ($this->activity_id == 0) {
       Message::setMsg("Please select an activity", "error");
       return false;
     } 
+    // user did not select a booking date
     if ($this->booking_date == 0) {
       Message::setMsg("Please select a date", "error");
       return false;
     } 
+    // user selected a booking date less than the current date
     if ($this->booking_date < $date_today) {
       Message::setMsg("Please select a valid date", "error");
       return false;
     }
+    // user did not select a start time
     if ($this->start_time == 0) {
       Message::setMsg("Please select a start time", "error");
       return false;
     } 
+    // user selected a start time less than the current time for a date in the past
     if ($this->start_time <= $time_today && $this->booking_date <= $date_today) {
       Message::setMsg("Please select a valid start time", "error");
       return false;
     } 
+    // user did not select an end time
     if ($this->end_time == 0) {
       Message::setMsg("Please select an end time", "error");
       return false;
     } 
+    // user selected an end time less than the start time
     if ($this->end_time <= $this->start_time) {
       Message::setMsg("Please select a valid end time", "error");
       return false;
     } 
+    // user selected an end time less than the current time for a date in the past
     if ($this->end_time <= $time_today && $this->booking_date <= $date_today) {
       Message::setMsg("Please select a valid end time", "error");
       return false;
     } 
+    // the salon is closed for the selected booking date and start time
     if ($this->salon_open() == false) {
       Message::setMsg("The salon is closed for this date and/or time", "error");
       return false;
     }
+    // the selected booking date and time does not fall within any of the selected hairdresser's schedules
     if ($this->schedule_open() == false) {
       Message::setMsg("The hairdresser is not working for this date and/or time", "error");
       return false;
     }
-     if ($this->booking_open() == false) {
+    // the selected booking date and times clashes with an existing booking
+    if ($this->booking_open() == false) {
       Message::setMsg("There is a clash with an existing booking", "error");
       return false;
     }
@@ -73,6 +85,7 @@ class Booking extends Db_object {
   }
   
   
+  // checks for open times 
   protected function salon_open() {
     $day = date("w", strtotime($this->booking_date));
     $day = $day + 1;
@@ -90,6 +103,7 @@ class Booking extends Db_object {
   } 
   
   
+  // checks for an available hairdresser schedule
   protected function schedule_open() {
     $day = date("w", strtotime($this->booking_date));
     $day = $day + 1;
@@ -109,6 +123,7 @@ class Booking extends Db_object {
   }
   
   
+  // checks for an available booking slot 
   protected function booking_open() {
     
     $sql = "SELECT * FROM bookings WHERE ";
--- admin/includes/class_guest_booking.php
+++ admin/includes/class_guest_booking.php
@@ -10,53 +10,64 @@ class Guest_booking extends Booking {
   public $service_id;
   
   
-  // validate form inputs  
+  // validate form inputs and check avalability
   public function validate() {
     date_default_timezone_set("Europe/London");
     $date_today = date("Y-m-d");
     $time_today = date("H:i:s");
     
+    // user did not select a hairdresser
     if ($this->hairdresser_id == 0) {
       Message::setMsg("Please select a hairdresser", "error");
       return false;
     } 
+    // user did not select a booking date
     if ($this->booking_date == 0) {
       Message::setMsg("Please select a date", "error");
       return false;
     } 
+    // user selected a booking date less than the current date
     if ($this->booking_date < $date_today) {
       Message::setMsg("Please select a valid date", "error");
       return false;
     }
+    // user did not select a start time
     if ($this->start_time == 0) {
       Message::setMsg("Please select a start time", "error");
       return false;
     } 
+    // user selected a start time less than the current time for a date in the past
     if ($this->start_time <= $time_today && $this->booking_date <= $date_today) {
       Message::setMsg("Please select a valid start time", "error");
       return false;
     } 
+    // user did not select a service
     if ($this->service_id == 0) {
       Message::setMsg("Please select a service", "error");
       return false;
     } 
+    // user did not provide the guest's name
     if ($this->guest_name == '') {
       Message::setMsg("Please provide the guest's name", "error");
       return false;
     } 
+    // user did not provide the guest's tel
     if ($this->guest_tel == '') {
       Message::setMsg("Please provide the guest's tel", "error");
       return false;
     } 
+    // the salon is closed for the selected booking date and start time
     if ($this->salon_open() == false) {
       Message::setMsg("The salon is closed for this date and/or time", "error");
       return false;
     }
+    // the selected booking date and time does not fall within any of the selected hairdresser's schedules
     if ($this->schedule_open() == false) {
       Message::setMsg("The hairdresser is not working for this date and/or time", "error");
       return false;
     }
-     if ($this->booking_open() == false) {
+    // the selected booking date and times clashes with an existing booking
+    if ($this->booking_open() == false) {
       Message::setMsg("There is a clash with an existing booking", "error");
       return false;
     }
@@ -65,7 +76,7 @@ class Guest_booking extends Booking {
   }
   
   
-  // validate against hairdresser schedules
+  // checks for an available hairdresser schedule
   protected function schedule_open() {
     $service = Service::find_by_id($this->service_id);
     $minutes = minutes($service->duration);
