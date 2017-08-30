3c637d22c7b703cd0ccabfe00385e65a5b21c38b
Peter Cheung <perverskitty@gmail.com>
2017-08-30 14:28:07 +0100

    Renaming admin/bookings_guests.php to admin/salon.guest-bookings.php.

--- admin/salon.guest-bookings.php
+++ admin/salon.guest-bookings.php
@@ -1,7 +1,8 @@
 <?php include("includes/header.php"); ?>
      
 <?php if(!$session->is_signed_in()) { redirect("signin.php"); } ?>
-<?php if($session->user_role != 1 && $session->user_role != 2) { redirect("index.php"); } ?>
+<?php if($session->user_role != 1 && $session->user_role != 2 && $session->user_role != 3) { redirect("signout.php"); } ?> 
+<?php if($session->user_role == 3) { redirect("clients.index.php"); } ?>
      
 <?php
 
@@ -44,7 +45,7 @@ $guest_bookings = Guest_booking::find_by_query($sql);
         </div> <!-- end of search bookings input -->
         <div class="flextable-item">
           <div class="btn-group">
-            <button type="button" class="btn btn-outline-primary" onclick="window.location='booking_guest_add.php'">
+            <button type="button" class="btn btn-outline-primary" onclick="window.location='salon.add-guest-booking.php'">
               <span class="icon icon-plus"></span> Add guest booking
             </button>
           </div>
@@ -69,7 +70,7 @@ $guest_bookings = Guest_booking::find_by_query($sql);
           <tbody>
           <?php foreach ($guest_bookings as $guest_booking) : ?>
             <tr>
-              <td><a href="client_edit.php?id=<?php echo $guest_booking->id; ?>"><span class="icon icon-edit"></span></a></td>
+              <td><a href="salon.edit-client.php?id=<?php echo $guest_booking->id; ?>"><span class="icon icon-edit"></span></a></td>
               <td><?php echo $guest_booking->id; ?></td>
               <td><?php echo date("D, j M Y", strtotime($guest_booking->booking_date)); ?></td>
               <td><?php echo substr($guest_booking->start_time, 0, 5)." - ".substr($guest_booking->end_time, 0, 5); ?></td>
@@ -91,20 +92,20 @@ $guest_bookings = Guest_booking::find_by_query($sql);
           <?php
             
           if ($paginate->has_previous()) {
-            echo "<li class='page-item'><a class='page-link' href='bookings_guests.php?page={$paginate->previous()}' aria-label='Previous'><span aria-hidden='true'>&laquo;</span><span class='sr-only'>Previous</span></a></li>";
+            echo "<li class='page-item'><a class='page-link' href='salon.guest-bookings.php?page={$paginate->previous()}' aria-label='Previous'><span aria-hidden='true'>&laquo;</span><span class='sr-only'>Previous</span></a></li>";
           }
            
           for ($i=1; $i <= $paginate->total_pages(); $i++) {
             if($i == $paginate->current_page) {
-              echo "<li class='page-item active'><a class='page-link' href='bookings_guests.php?page={$i}'>{$i}</a></li>";
+              echo "<li class='page-item active'><a class='page-link' href='salon.guest-bookings.php?page={$i}'>{$i}</a></li>";
             } else {
-              echo "<li class='page-item'><a class='page-link' href='bookings_guests.php?page={$i}'>{$i}</a></li>";
+              echo "<li class='page-item'><a class='page-link' href='salon.guest-bookings.php?page={$i}'>{$i}</a></li>";
             }
           } 
             
           if ($paginate->total_pages() > 1) {
             if ($paginate->has_next()) {
-              echo "<li class='page-item'><a class='page-link' href='bookings_guests.php?page={$paginate->next()}' aria-label='Next'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>";
+              echo "<li class='page-item'><a class='page-link' href='salon.guest-bookings.php?page={$paginate->next()}' aria-label='Next'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>";
             }
           }
           
