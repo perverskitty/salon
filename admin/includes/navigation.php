06e47b810e6bdfcea1cd07e3eedf4e66bf5e56fb
Peter Cheung <perverskitty@gmail.com>
2017-08-30 13:09:09 +0100

    Updating nav links access based on user role in admin/includes/navigation.php.

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -14,55 +14,64 @@
         </div>
         
         <?php if($session->is_signed_in()) : ?>
-        
-<!--
-        <div class="sidebar-header">
-          <h6><?php //echo date("D, j M Y"); ?></h6>
-        </div>
--->
          
         <!-- sidebar search and list of links collapsable -->
         <div class="collapse nav-toggleable-md" id="nav-toggleable-md">
           
           <ul class="nav nav-pills nav-stacked flex-column">
             <li class="nav-header">Dashboards</li>
+            
+            <?php if($session->user_role == 3) : ?>
             <li class="nav-item">
-              <a class="nav-link" href="index.php"><?php echo $session->user_fname."'s Dashboard"; ?></a>
+              <a class="nav-link" href="clients.index.php">Home</a>
             </li>
+            <?php endif; ?>
+            
             <?php if($session->user_role == 1 || $session->user_role == 2) : ?>
             <li class="nav-item">
-              <a class="nav-link" href="overview.php">Salon Dashboard</a>
+              <a class="nav-link" href="salon.index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="salon.overview.php">Salon Dashboard</a>
             </li>
             <li class="nav-header">Admin</li>
             <li class="nav-item">
-              <a class="nav-link" href="clients.php">Clients</a>
+              <a class="nav-link" href="salon.clients.php">Clients</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link"href="hairdressers.php">Hairdressers</a>
+              <a class="nav-link"href="salon.hairdressers.php">Hairdressers</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="schedules.php">Schedules</a>
+              <a class="nav-link" href="salon.schedules.php">Schedules</a>
             </li>
               <li class="nav-item">
-                <a class="nav-link" href="services.php">Services</a>
+                <a class="nav-link" href="salon.services.php">Services</a>
               </li>
             <li class="nav-item">
-              <a class="nav-link" href="bookings.php">All Bookings</a>
+              <a class="nav-link" href="salon.bookings.php">All Bookings</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="bookings_clients.php">Client Bookings</a>
+              <a class="nav-link" href="salon.bookings_clients.php">Client Bookings</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="bookings_guests.php">Guest Bookings</a>
+              <a class="nav-link" href="salon.bookings_guests.php">Guest Bookings</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="bookings_other.php">Other Bookings</a>
+              <a class="nav-link" href="salon.bookings_other.php">Other Bookings</a>
             </li>
-            <?php endif; ?>
             <li class="nav-header">My Account</li>
             <li class="nav-item">
-              <a class="nav-link" href="profile.php"><?php echo $session->user_fname."'s Account"; ?></a>
+              <a class="nav-link" href="salon.profile.php"><?php echo $session->user_fname."'s Account"; ?></a>
             </li>
+            <?php endif; ?>
+            
+            <?php if($session->user_role == 3) : ?>
+            <li class="nav-header">My Account</li>
+            <li class="nav-item">
+              <a class="nav-link" href="clients.profile.php"><?php echo $session->user_fname."'s Account"; ?></a>
+            </li>
+            <?php endif; ?>
+            
             <li class="nav-item">
               <a class="nav-link" href="signout.php">Sign Out</a>
             </li>
