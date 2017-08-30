5db41f386014d9490dfe818914c1dd2fd9d2a542
Peter Cheung <perverskitty@gmail.com>
2017-08-30 15:45:11 +0100

    Renaming admin/booking_other_add.php to admin/salon.add-other-booking.php.

--- admin/salon.add-other-booking.php
+++ admin/salon.add-other-booking.php
@@ -1,7 +1,9 @@
 <?php include("includes/header.php"); ?>
      
 <?php if(!$session->is_signed_in()) { redirect("signin.php"); } ?>
-<?php if($session->user_role != 1) { redirect("index.php"); } ?>
+<?php if($session->user_role != 1 && $session->user_role != 2 && $session->user_role != 3) { redirect("signout.php"); } ?> 
+<?php if($session->user_role == 3) { redirect("clients.index.php"); } ?>
+<?php if($session->user_role == 2) { redirect("salon.index.php"); } ?>
      
 <?php
 
@@ -24,7 +26,7 @@ if (isset($_POST['create'])) {
     if ($booking->validate()) {
       
       if ($booking->save()) { 
-        redirect("bookings_other.php"); 
+        redirect("salon.other-bookings.php"); 
       }
       
     }
